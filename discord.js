fb73ac6263e066bdc1287b9dc3838cd4bc5c01e8
HaileyPong <haileypong@berkeley.edu>
2023-08-24 14:57:20 -0400

    updated env variable

--- discord.js
+++ discord.js
@@ -3,20 +3,6 @@ const {Client, GatewayIntentBits, EmbedBuilder, PermissionBitField, Permissions}
 const discordClient = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
 const channel = discordClient.channels.cache.get("1143646553093984428");
 
-/*
-const url = `<YOUR WEBHOOK URL>`;
-const fetch = require('node-fetch');
-
-function sendMessage(message) {
-   fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({"username": "Hashnode Bot", "content": `New blog post 👉 [${message.title}](${message.url})`})
-    });
-}
-*/
 discordClient.on("ready", (x) => {
 	console.log(`${x.user.tag} is ready!`);
 	
